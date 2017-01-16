ae1d2d89e28685fd0db0412ce7d0065cb114d925
Caitlin Cassidy <ccassidy@mit.edu>
2017-01-16 14:30:18 -0500

    restricted what you can change on admin for Events

--- attendance/admin.py
+++ attendance/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Event, Sister
 
+class EventAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': ['name', 'date', 'is_mandatory', 'points']})
+  ]
+  list_display = ('name', 'date', 'is_mandatory', 'points')
 
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Sister)
