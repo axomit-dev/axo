108b5c37c4291ac5205ca3668bdc28f987917333
Caitlin Cassidy <ccassidy@mit.edu>
2017-01-18 09:33:16 -0500

    added password reset and password change features

--- axo/urls.py
+++ axo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from general import views as general_views
 #from django.urls import reverse
+from settings import LOGIN_REDIRECT_URL
 
 
 urlpatterns = [
@@ -26,9 +27,20 @@ urlpatterns = [
 #url('^', include('django.contrib.auth.urls')),
 url(r'^login/', auth_views.login, name='login'),
 url(r'^logout/', auth_views.logout, name='logout'),
-url(r'^change-password/$', auth_views.password_change,
-    {'post_change_redirect': 'attendance:personal_record'},
+url(r'^password_change/$',
+    auth_views.password_change,
+    {'post_change_redirect': LOGIN_REDIRECT_URL},
     name='password_change'),
+url(r'^password_reset/$',
+    auth_views.password_reset,
+    name='password_reset'),
+url(r'^password_reset_done/$',
+    auth_views.password_reset_done,
+    name='password_reset_done'),
+url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    auth_views.password_reset_confirm,
+    {'post_reset_redirect': LOGIN_REDIRECT_URL},
+    name='password_reset_confirm'),
 
 url(r'^polls/', include('polls.urls')),
 url(r'^admin/', admin.site.urls),
