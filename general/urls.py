234ddb716e027e852a488c4599673cca447d005b
Rebecca Weinberger <rew@mit.edu>
2017-01-18 03:00:10 -0500

    minor changes & added a credits link at bottom

--- general/urls.py
+++ general/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 
   url(r'^$', views.index, name='index'),
+  url(r'^credits/$', views.credits, name='credits'),
 ]
--- general/views.py
+++ general/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
   return render(request, 'general/index.html', {})
+
+def credits(request):
+  return render(request, 'general/credits.html', {})
