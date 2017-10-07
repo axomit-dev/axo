c2ab36ce24c38434d89ac5a23adc40f102482d9a
Caitlin Cassidy <ccassidy@mit.edu>
2017-10-07 12:05:15 -0400

    Changed ElectionSettings admin page so it's more intuitive of what to click.

--- elections/admin.py
+++ elections/admin.py
@@ -2,16 +2,13 @@ from django.contrib import admin
 
 from .models import ElectionSettings, Office, OfficeInterest, Loi
 
-class ElectionSettingsAdmin(admin.ModelAdmin):
-  list_display = ('exec_election', 'ois_open', 'ois_results_open', 'loi_open', 'slating_open', 'senior_class_year')
-
 class OfficeInterestAdmin(admin.ModelAdmin):
   list_display = ('sister', 'office', 'interest')
 
 class LoiAdmin(admin.ModelAdmin):
   list_display = ('office', 'names_of_sisters', 'loi_text')
 
-admin.site.register(ElectionSettings, ElectionSettingsAdmin)
+admin.site.register(ElectionSettings)
 admin.site.register(Office)
 admin.site.register(OfficeInterest, OfficeInterestAdmin)
 admin.site.register(Loi, LoiAdmin)
