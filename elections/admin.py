e012c1fca5e51711b863e10914a33a9555249281
axo <axo@cats-whiskers.mit.edu>
2017-10-07 17:24:48 -0400

    Add Office admin view

--- elections/admin.py
+++ elections/admin.py
@@ -5,6 +5,9 @@ from .models import ElectionSettings, Office, OfficeInterest, Loi
 class ElectionSettingsAdmin(admin.ModelAdmin):
   list_display = ('exec_election', 'ois_open', 'ois_results_open', 'loi_open', 'slating_open', 'senior_class_year')
 
+class OfficeAdmin(admin.ModelAdmin):
+  list_display = ('title', 'is_exec', 'eligible_class')
+
 class OfficeInterestAdmin(admin.ModelAdmin):
   list_display = ('sister', 'office', 'interest')
 
@@ -12,6 +15,6 @@ class LoiAdmin(admin.ModelAdmin):
   list_display = ('office', 'names_of_sisters', 'loi_text')
 
 admin.site.register(ElectionSettings, ElectionSettingsAdmin)
-admin.site.register(Office)
+admin.site.register(Office, OfficeAdmin)
 admin.site.register(OfficeInterest, OfficeInterestAdmin)
 admin.site.register(Loi, LoiAdmin)
