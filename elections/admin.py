95ed2e892974c5616177c83b0650aa4d9b2ba851
Caitlin Cassidy <ccassidy@mit.edu>
2017-11-02 23:18:02 -0400

    Add VotingSetting to admin page.

--- elections/admin.py
+++ elections/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ElectionSettings, Office, OfficeInterest, Loi, Slate, FinalVote, FinalVoteParticipant
+from .models import ElectionSettings, Office, OfficeInterest, Loi, Slate, FinalVote, FinalVoteParticipant, VotingSetting
 
 class OfficeAdmin(admin.ModelAdmin):
   list_display = ('title', 'is_exec', 'eligible_class')
@@ -19,3 +19,4 @@ admin.site.register(Slate)
 # TODO: Give better display of FinalVote + FinalVoteParticipant
 admin.site.register(FinalVote)
 admin.site.register(FinalVoteParticipant)
+admin.site.register(VotingSetting)
